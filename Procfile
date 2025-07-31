48f1b5a42d3784c1869e3de69ba64266bc095d04
ksstrat <ksstratmann@gmail.com>
2025-07-31 23:42:49 +0200

    chore: Create Procfile for Heroku deployment and add it to ALLOWED_HOSTS.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pixelpuls.wsgi
--- pixelpulse/settings.py
+++ pixelpulse/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+o1n7=1)oh+i!h@5a%1lkx(nl0gg4*=^(kjyqovf*jeo*3clct'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
