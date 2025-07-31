b712d0cdf34230235b9b40f3d9506e86c7d53b86
ksstrat <ksstratmann@gmail.com>
2025-07-31 22:47:16 +0200

    feat: Define Category model.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+    description = models.TextField(blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
