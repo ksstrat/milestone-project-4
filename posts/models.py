845a5e03c9c0841da94103302d41d9da098ec9f5
ksstrat <ksstratmann@gmail.com>
2025-07-31 22:57:32 +0200

    feat: Define Post model and add comments.

--- posts/models.py
+++ posts/models.py
@@ -1,14 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils.text import slugify
 
 class Category(models.Model):
+    """
+    Model representing a category for posts.
+    """
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True, blank=True)
     description = models.TextField(blank=True)
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
+        """
+        Auto-generates a slug from the category name.
+        """
+        if not self.slug:
+            self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
+
+class Post(models.Model):
+    """
+    Model representing a user-created post on PixelPulse.
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField(blank=True)
+    url = models.URLField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=((0, 'Draft'), (1, 'Published')), default=0)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        """
+        Automatically generates a slug from the title if it's not set.
+        """
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
