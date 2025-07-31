1433e416f42ee77253bfec6ca5ec80563d05b7f5
ksstrat <ksstratmann@gmail.com>
2025-08-01 00:22:19 +0200

    chore: Fix Heroku deployment blank page by configuring ALLOWED_HOSTS.

--- pixelpulse/settings.py
+++ pixelpulse/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-+o1n7=1)oh+i!h@5a%1lkx(nl0gg4*=^(kjyqovf*jeo*3clct
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['pixelpulse.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
