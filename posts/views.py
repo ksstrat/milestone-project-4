37809da24df02a56f77c2922b5fac75eb9d242cd
ksstrat <ksstratmann@gmail.com>
2025-08-02 00:10:10 +0200

    feat: Create single post detail view.

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostList(ListView):
@@ -10,3 +10,11 @@ class PostList(ListView):
     model = Post
     template_name = "posts/index.html"
     context_object_name = 'post_list'
+
+class PostDetail(DetailView):
+    """
+    Displays the details of a single post.
+    """
+    model = Post
+    template_name = 'posts/post_detail.html'
+    context_object_name = 'post'
