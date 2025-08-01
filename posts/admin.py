756e6d75a486794247c52f176478bcd333e82e7d
ksstrat <ksstratmann@gmail.com>
2025-08-02 00:33:24 +0200

    feat: Customize admin panel for Post model.

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Category, Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_at')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
+
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
