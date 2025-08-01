4dc462991a1bb2a7366c78b5fc5fb8e523576df2
ksstrat <ksstratmann@gmail.com>
2025-08-01 23:09:56 +0200

    feat: Create homepage list view.

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Post
 
-def hello_world(request):
-    return HttpResponse("Hello, World!")
+class PostList(ListView):
+    """
+    Displays a list of posts on the homepage.
+    """
+    model = Post
+    template_name = "posts/index.html"
+    context_object_name = 'post_list'
