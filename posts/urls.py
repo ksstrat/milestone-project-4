f3ddaa9b29d4f6b892cbbf7f4ce5289d112442be
ksstrat <ksstratmann@gmail.com>
2025-08-01 23:27:22 +0200

    feat: Update URL configuration to use new PostList view.

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import PostList
 
 urlpatterns = [
-    path('', views.hello_world, name='hello_world'),
+    path('', PostList.as_view(), name='home'),
 ]
