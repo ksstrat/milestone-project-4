97ab81b0a9e034aaa834f274ca953c430bc751a9
ksstrat <ksstratmann@gmail.com>
2025-08-03 13:33:10 +0200

    feat: Configure Summernote for Post model in admin.

--- posts/admin.py
+++ posts/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from .models import Category, Post, Comment, Vote
 from django_summernote.admin import SummernoteModelAdmin
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_at')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_at')
     prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
