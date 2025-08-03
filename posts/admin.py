b1832aed10b9596e8b3b96fe9453c7ee680bce71
ksstrat <ksstratmann@gmail.com>
2025-08-03 13:29:55 +0200

    feat: Register Comment and Vote models in admin panel.

--- posts/admin.py
+++ posts/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
-from .models import Category, Post
+from .models import Category, Post, Comment, Vote
+from django_summernote.admin import SummernoteModelAdmin
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_at')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_at')
     prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('author__username', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'vote_type')
+    list_filter = ('vote_type',)
+    search_fields = ('user__username', 'post__title')
 
 admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
