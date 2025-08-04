35d1fbe5c820d3bc473ff8d2ceef2a37d937fb89
ksstrat <ksstratmann@gmail.com>
2025-08-05 01:23:42 +0200

    chore: Set secure SECRET_KEY

--- pixelpulse/settings.py
+++ pixelpulse/settings.py
@@ -25,7 +25,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+o1n7=1)oh+i!h@5a%1lkx(nl0gg4*=^(kjyqovf*jeo*3clct'
+# SECRET_KEY = 'django-insecure-+o1n7=1)oh+i!h@5a%1lkx(nl0gg4*=^(kjyqovf*jeo*3clct'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
