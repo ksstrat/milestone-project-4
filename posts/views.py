f6bfba97bec9fd270a35cec3a85a857876f25eea
ksstrat <ksstratmann@gmail.com>
2025-08-04 21:27:35 +0200

    feat: Add logic to PostDetail view for comments.

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
-from .models import Post
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Post, Comment
+from .forms import CommentForm
 
 class PostList(ListView):
     """
@@ -18,3 +19,21 @@ class PostDetail(DetailView):
     model = Post
     template_name = 'posts/post_detail.html'
     context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comments.all().order_by('-created_on')
+        context['comment_form'] = CommentForm()
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = self.object
+            new_comment.author = request.user
+            new_comment.save()
+
+        return HttpResponseRedirect(reverse('post_detail', kwargs={'slug': self.object.slug}))
