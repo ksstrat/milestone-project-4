c0ef8a447f48e2bb619d1fe2f485e68234c19ae3
ksstrat <ksstratmann@gmail.com>
2025-08-06 18:54:55 +0200

    feat: Add URL for post deletion.

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostList, PostDetail, VoteView, PostCreate, PostUpdate
+from .views import PostList, PostDetail, VoteView, PostCreate, PostUpdate, PostDelete
 
 urlpatterns = [
     path('', PostList.as_view(), name='home'),
     path('create/', PostCreate.as_view(), name='post_create'),
     path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
     path('edit/<slug:slug>/', PostUpdate.as_view(), name='post_edit'),
+    path('delete/<slug:slug>/', PostDelete.as_view(), name='post_delete'),
     path('vote/<slug:slug>/', VoteView.as_view(), name='vote'),
 ]
