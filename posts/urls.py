029b28324d58382308975a1a0a9f743bcab2f642
ksstrat <ksstratmann@gmail.com>
2025-08-06 16:09:37 +0200

    feat: Add URL for post editing.

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostList, PostDetail, VoteView, PostCreate
+from .views import PostList, PostDetail, VoteView, PostCreate, PostUpdate
 
 urlpatterns = [
     path('', PostList.as_view(), name='home'),
     path('create/', PostCreate.as_view(), name='post_create'),
     path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
+    path('edit/<slug:slug>/', PostUpdate.as_view(), name='post_edit'),
     path('vote/<slug:slug>/', VoteView.as_view(), name='vote'),
 ]
