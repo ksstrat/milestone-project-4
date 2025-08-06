907aec6c0525aa61810936807c9cd72a21784d74
ksstrat <ksstratmann@gmail.com>
2025-08-06 14:52:59 +0200

    fix: Add missing CreateView import in posts/views.py.

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.views import View
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from django.shortcuts import redirect, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
