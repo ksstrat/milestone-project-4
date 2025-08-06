075894ab2a2b04962db47c684f9e088df833ebf0
ksstrat <ksstratmann@gmail.com>
2025-08-06 14:23:40 +0200

    feat: Create PostForm for content creation.

--- posts/forms.py
+++ posts/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 from django_summernote.widgets import SummernoteWidget
-from .models import Comment, Vote
+from .models import Comment, Vote, Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'category', 'content', 'featured_image', 'url')
+        widgets = {
+            'content': SummernoteWidget(),
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
