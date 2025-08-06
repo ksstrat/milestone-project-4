4adf608ba50f9179018ce2e185ad33eb20c8515e
ksstrat <ksstratmann@gmail.com>
2025-08-06 15:10:11 +0200

    fix: Correct URL pattern order in posts/urls.py.

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from .views import PostList, PostDetail, VoteView, PostCreate
 
 urlpatterns = [
     path('', PostList.as_view(), name='home'),
+    path('create/', PostCreate.as_view(), name='post_create'),
     path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
     path('vote/<slug:slug>/', VoteView.as_view(), name='vote'),
-    path('create/', PostCreate.as_view(), name='post_create'),
 ]
