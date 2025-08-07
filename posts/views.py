154cee206e00b87155b721e795dc52bd531f10da
ksstrat <ksstratmann@gmail.com>
2025-08-07 16:07:15 +0200

    feat: Pass categories to navigation template.

--- posts/views.py
+++ posts/views.py
@@ -33,6 +33,11 @@ class PostList(ListView):
             queryset = queryset.filter(category__name=category)
 
         return queryset.filter(status=1).order_by('-created_at')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 class PostDetail(DetailView):
     """
