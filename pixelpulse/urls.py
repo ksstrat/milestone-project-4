d8dfe08d7808215b95f24bacb71e9af620445ddc
ksstrat <ksstratmann@gmail.com>
2025-08-11 22:57:48 +0200

    chore: Add /privacy and /data-deletion routes.

--- pixelpulse/urls.py
+++ pixelpulse/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('privacy/', TemplateView.as_view(template_name='privacy.html'), name='privacy'),
+    path('data-deletion/', TemplateView.as_view(template_name='data_deletion.html'), name='data_deletion'),
     path('accounts/', include('allauth.urls')),
     path('', include('posts.urls')),
 ]
